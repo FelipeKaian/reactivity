b9ef0ebd34df0e308cbfa866f8e8991727437f65
FelipeKaian <felipekaianmutti@gmail.com>
2024-06-01 13:49:48 -0300

    wip

--- lib/reactive_status.dart
+++ lib/reactive_status.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'reactive_manager.dart';
 
 class ReactiveStatus<T> extends StatefulWidget {
-  ReactiveStatus(this.cases,
+  const ReactiveStatus(this.cases,
       {super.key, this.keys = const [Null], this.defaultCase});
 
   final Map<T, Widget Function()> cases;
   final Widget Function()? defaultCase;
   final List<dynamic> keys;
-  final Key watcherKey = UniqueKey();
 
   @override
   State<ReactiveStatus<T>> createState() => _ReactiveStatus<T>();
